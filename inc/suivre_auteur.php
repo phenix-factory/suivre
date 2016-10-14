57b4c94ea68ae43f27a1ed8e5b9688eb85b6eb7e
Debondt Didier <p@henix.be>
2016-10-14 20:49:17 +0200

    Emplacement plus logique pour cette fonction

--- inc/suivre_auteur.php
+++ inc/suivre_auteur.php
@@ -4,6 +4,8 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
+include_spip('suivre_fonctions');
+
 /**
  * Suivre un auteur
  *
@@ -23,29 +25,12 @@ function inc_suivre_auteur_dist($id_auteur_suivre, $id_auteur_source = null) {
 	);
 }
 
-/**
- * Test l'existence d'un id_auteur. Sinon, va chercher celui de la session
- *
- * @param mixed $id_auteur
- * @access public
- * @return mixed
- */
-function id_auteur_session($id_auteur) {
-
-	if (is_null($id_auteur_source)) {
-		include_spip('inc/session');
-		return session_get('id_auteur');
-	} else {
-		return $id_auteur;
-	}
-}
-
 function quitter_auteur($id_auteur_suivre, $id_auteur_source = null) {
 
 	include_spip('action/editer_liens');
 	$id_auteur_source = id_auteur_session($id_auteur_source);
 
-	return objet_dissocier(
+	return objet_associer(
 		array('auteur' => $id_auteur_source),
 		array('auteur' => $id_auteur_suivre)
 	);
--- suivre_fonctions.php
+++ suivre_fonctions.php
@@ -9,4 +9,23 @@
  * @package    SPIP\Suivre\Fonctions
  */
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+/**
+ * Test l'existence d'un id_auteur. Sinon, va chercher celui de la session
+ *
+ * @param mixed $id_auteur
+ * @access public
+ * @return mixed
+ */
+function id_auteur_session($id_auteur) {
+
+	if (is_null($id_auteur_source)) {
+		include_spip('inc/session');
+		return session_get('id_auteur');
+	} else {
+		return $id_auteur;
+	}
+}
