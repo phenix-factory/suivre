f3b4f78de425a2e8258711943d77584992d96e3b
Debondt Didier <p@henix.be>
2016-10-14 16:22:34 +0200

    Début de la fonction d'autorisation

--- suivre_autorisations.php
+++ suivre_autorisations.php
@@ -17,3 +17,14 @@ if (!defined('_ECRIRE_INC_VERSION')) {
  * Fonction d'appel pour le pipeline
  * @pipeline autoriser */
 function suivre_autoriser(){}
+
+function autoriser_suivre_auteur_dist($faire, $type, $id, $qui, $opt) {
+
+	// Il faut être un véritable auteur
+	if ($qui['id_auteur'] !== 0) {
+		return false;
+	}
+
+	// On ne peu pas suivre quelqu'un que l'on suis déjà
+	include_spip('inc/editer_liens');
+}
